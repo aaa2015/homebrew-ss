5cd78b040bbbc4217c5f896e961111c31e7b3398
ayuan1129 <ayuan1129@qq.com>
2026-02-01 10:45:49 +0800

    Update sha256

--- Formula/ss.rb
+++ Formula/ss.rb
@@ -5,7 +5,7 @@ class Ss < Formula
   desc "Socket statistics tool for macOS (Linux ss command clone)"
   homepage "https://github.com/aaa2015/myiosss"
   url "https://github.com/aaa2015/myiosss/archive/v1.0.1.tar.gz"
-  sha256 "785d98490cd78dcb56b0d7240e8de19594f1de8671a3e159fff5729a6406b2ab"
+  sha256 "2696a0a7f949d892e9b2a59a34ef773bf1fd1bc4c74961c498e6656f6c652d1e"
   license "MIT"
   version "1.0.1"
 
