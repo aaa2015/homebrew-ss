332cae57886a22a67d66108ef8cc5c305dfd1165
ayuan1129 <ayuan1129@qq.com>
2026-07-19 16:45:05 +0800

    Update SHA-256 for mycode 0.1.16 repackage (removed models)

--- Formula/mycode.rb
+++ Formula/mycode.rb
@@ -77,7 +77,7 @@ class Mycode < Formula
     on_arm do
       url "https://github.com/aaa2015/mycode/releases/download/#{version}/mycode-#{version}-aarch64-apple-darwin.tar.gz",
           using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "c7cd7871a03474968743bcdace92b2b158c665c6cac8305e1168d3acb66d345f"
+      sha256 "bcaadeebec2dc2298f49a1389d7ac73ed095475a1fa48e7c3a01cf55abf8d379"
     end
   end
 
